6c89f420ab9ed3bfe6384030bdf617cbafd7fb1d
Erick <santoserick9@gmail.com>
2021-08-11 23:15:08 -0300

    added object array to Map component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,5 +3,19 @@ import dynamic from 'next/dynamic'
 const Map = dynamic(() => import('components/Map'), { ssr: false })
 
 export default function Home() {
-  return <Map />
+	return (
+		<Map
+			places={[
+				{
+					id: '2',
+					name: 'Olinda',
+					slug: 'olinda',
+					location: {
+						latitude: 45,
+						longitude: -110
+					}
+				}
+			]}
+		/>
+	)
 }
