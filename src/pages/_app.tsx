ba0dcff106d57dd3e22e6dc6dac50427454dee56
Erick <santoserick9@gmail.com>
2021-08-12 16:56:11 -0300

    added @styled-icons whitin the project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,30 +4,30 @@ import Head from 'next/head'
 import GlobalStyles from 'styles/global'
 
 function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>Blog - My Trips</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#06092B" />
-        <meta
-          name="description"
-          content="Um simples projeto para mostrar meus lugares favoritos pelo mundo."
-        />
+	return (
+		<>
+			<Head>
+				<title>Blog - My Trips</title>
+				<link rel="shortcut icon" href="/img/icon-512.png" />
+				<link rel="apple-touch-icon" href="/img/icon-512.png" />
+				<link rel="manifest" href="/manifest.json" />
+				<meta name="theme-color" content="#06092B" />
+				<meta
+					name="description"
+					content="Um simples projeto para mostrar meus lugares favoritos pelo mundo."
+				/>
 
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-          integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-          crossOrigin=""
-        />
-      </Head>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </>
-  )
+				<link
+					rel="stylesheet"
+					href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+					integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+					crossOrigin=""
+				/>
+			</Head>
+			<GlobalStyles />
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default App
