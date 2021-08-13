3713852d157b6a0923a55db86b19e120f7cc31e6
Erick <santoserick9@gmail.com>
2021-08-13 15:49:37 -0300

    fix: remove colon from bearer authorization

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -4,7 +4,7 @@ const endpoint = process.env.GRAPHQL_HOST || ''
 
 const client = new GraphQLClient(endpoint, {
 	headers: {
-		authorization: `Bearer: ${process.env.GRAPHQL_TOKEN}`
+		Authorization: `Bearer ${process.env.GRAPHQL_TOKEN}`
 	}
 })
 
