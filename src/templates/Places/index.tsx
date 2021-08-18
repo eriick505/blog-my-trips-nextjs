9f66c91f843a6fe4eaebbb6f19334fc89e9f6194
Erick <santoserick9@gmail.com>
2021-08-17 23:41:05 -0300

    feat: add image next component, fix problem typing and fallback

--- src/templates/Places/index.tsx
+++ src/templates/Places/index.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image'
+import { useRouter } from 'next/dist/client/router'
+
 import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline'
 import LinkWrapper from 'components/LinkWrapper'
 
@@ -14,7 +17,7 @@ export type PlacesTemplateProps = {
 	place: {
 		slug: string
 		name: string
-		description: {
+		description?: {
 			html: string
 		}
 		gallery: ImageProps[]
@@ -22,6 +25,10 @@ export type PlacesTemplateProps = {
 }
 
 export default function PlacesTemplate({ place }: PlacesTemplateProps) {
+	const router = useRouter()
+
+	if (router.isFallback) return null
+
 	return (
 		<>
 			<LinkWrapper href="/">
@@ -33,13 +40,20 @@ export default function PlacesTemplate({ place }: PlacesTemplateProps) {
 					<S.Heading>{place.name}</S.Heading>
 
 					<S.Body
-						dangerouslySetInnerHTML={{ __html: place.description.html }}
+						dangerouslySetInnerHTML={{ __html: place.description?.html || '' }}
 					/>
 
 					<S.Gallery>
-						{/* {place.gallery.map((img, index) => (
-				<img key={`photo-${index}`} src={img.url} alt={img.fileName} />
-			))} */}
+						{place.gallery.map((img, index) => (
+							<Image
+								key={`photo-${index}`}
+								src={img.url}
+								alt={img.fileName}
+								width={500}
+								height={400}
+								quality={75}
+							/>
+						))}
 					</S.Gallery>
 				</S.Container>
 			</S.Wrapper>
